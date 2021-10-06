ee629742e02f6b483b917c247028a1011d6be7e3
Gabriel Babler <gabriel.babler1@hotmail.com>
2021-10-06 20:49:07 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,4 @@
+function validar(){
+    window.location.href = "https://www.youtube.com/watch?v=5qap5aO4i9A";
+}
+
---
+++ js/menu.js
@@ -0,0 +1,7 @@
+function listaAlunos(){
+    window.location.href = "https://www.youtube.com/watch?v=5qap5aO4i9A"//teste
+}
+
+function perfilMotorista(){
+    window.location.href = "https://www.youtube.com/channel/UCwWniP9WFynuslE3VCrjzCw"//teste
+}
